faf6c55762598bab16eb2aa037883a5133b2fbfa
vasco ceti <vasco.ceti@gmail.com>
2024-01-25 18:21:48 +0100

    .

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import img from "../assets/investment-calculator-logo.png";
-export default function Header() {
-  return (
-    <header id="header">
-      <img src={img} alt="header-img" />
-      <h1>Investment Calculator</h1>
-    </header>
-  );
-}
